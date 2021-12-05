bbf59f64368c28bb5c6f4c8928c029cb40aa64e5
jsuryahyd <jaysuryahyd@gmail.com>
2021-12-05 18:30:16 +0000

    more pointers practice

--- main.cpp
+++ main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
-#include "sum.h"
 
+void whatever(){
+   const char* args[3] = {"a","b","c32"};
+  size_t count = sizeof args / sizeof *args;
+
+    std::cout <<count << "\n" ;
+    std::cout << args  <<" ==== " << sizeof args << "\n";
+    std::cout << *args[2] <<" ==== "  << sizeof *args[2] << "\n";
+    std::cout << args[2] <<" ==== "  << sizeof args[2] << "\n";
+}
+
+void doSomething(const char* args[],size_t count){
+  for(int i=0;i<count;i++){
+    std::cout << *args[i] <<"\n";
+    std::cout << args[i] <<"\n";
+  }
+}
 
 int main() {
-    std::cout << "Hello, world!\n" ;
+const char* args[3] = {"a","b","c32"};
+  size_t count = sizeof args / sizeof *args;
+
+  whatever();
+  doSomething(args,count);
+  
     return 0;
 }
