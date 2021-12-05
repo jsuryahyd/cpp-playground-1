f54ed3b9d8058f409bd6ed440659d1250e20f17f
jsuryahyd <jaysuryahyd@gmail.com>
2021-12-05 09:17:07 +0000

    basic setup

--- main.cpp
+++ main.cpp
@@ -4,7 +4,5 @@
 
 int main() {
     std::cout << "Hello, world!\n" ;
-    std::cout << Arithmetic::sum(3,289) << std::endl;
-    std::cout << strMethods::sum(strMethods::sum("Bye","World"),"\n");
     return 0;
 }
--- sum.h
+++
@@ -1,9 +0,0 @@
-#pragma once // include only once
-
-namespace Arithmetic{
-  int sum(int a, int b);
-}
-
-namespace strMethods{
-  std::string sum(std::string a, std::string b);
-}
